5abd501f6e988c091c1d82f31d40e03db431066b
Tirthpanchal412 <tirthpanchal412@gmail.com>
2025-08-19 11:14:02 +0530

    Update 22.c

--- assignment-1/22.c
+++ assignment-1/22.c
@@ -7,7 +7,8 @@ int main()
     printf("Enter gross sales\n");
     scanf("%f", &gross_sales);
 
-    discount = gross_sales * 0.10; // 10% discount
+    discount = gross_sales * 0.10; 
+    
     net_sales = gross_sales - discount;
 
     printf("Net Sales = %.2f", net_sales);
